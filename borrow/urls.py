6f84bf04f0fc8828db9271af447e3d8601326cea
khalidyousry <yousryk350@gmail.com>
2026-05-10 01:54:36 +0300

    Fix borrow urls

--- borrow/urls.py
+++ borrow/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import my_borrows, return_book, borrowed_books_page
+from .views import my_borrows, return_book, borrowed_books_page, borrow_book
 
 urlpatterns = [
     path('my-borrows/', my_borrows, name='my_borrows'),
     path('return/<int:borrow_id>/', return_book, name='return_book'),
-
-    # template page
+    path('', borrow_book, name='borrow_book'),
     path('borrowed-books/', borrowed_books_page, name='borrowed_books_page'),
 ]
